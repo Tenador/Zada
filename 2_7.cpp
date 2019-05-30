44a1fb92cf399c66bda430a116bed70c168c0d89
Tenador <tenador111@gmail.com>
2019-05-30 16:10:41 +0300

    git commit

---
+++ 2_7.cpp
@@ -0,0 +1,14 @@
+#iclude <iostream>
+
+using namespace std;
+
+int main(){
+  int arr[n];
+  cout<<"Vvedita nyshnoe kol-vo chisel: ";
+  cin>>n;
+  int a, i, z;
+  for (i=0; i<n; i++){
+
+  }
+  return 0;
+}
